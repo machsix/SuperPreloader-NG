309977b286f30a4a7f1084a7f0dec06a617ff99f
machsix <28209092+machsix@users.noreply.github.com>
2024-06-09 02:24:27 -0400

    two pagination mode

--- autopager.js
+++ autopager.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        New script mach.workers.dev
+// @name        SuperPreloader-NG
 // @namespace   Violentmonkey Scripts
-// @match       https://xiutaku.mach.workers.dev/*
+// @include     http*
 // @grant       none
 // @version     1.0
 // @run-at      document-start
@@ -11,37 +11,42 @@
 
 (function () {
   function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async function fetchContent(url, contentSelector) {
+  async function fetchContent(page_link, pageElement, nextLink = null) {
     try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const htmlText = await response.text();
-            console.log(`Fetched ${url}`)
+      const response = await fetch(page_link);
+      if (response.ok) {
+        const htmlText = await response.text();
+        console.log(`Fetched ${page_link}`);
 
-            // Create a DOMParser
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(htmlText, 'text/html');
-            return doc.querySelector(contentSelector);
-        } else {
-            console.error(`Error fetching content from ${url}. Status code: ${response.status}`);
-        }
+        // Create a DOMParser
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlText, "text/html");
+        const content = doc.querySelector(pageElement);
+        const next_page_link =
+          nextLink === null ? null : doc.querySelector(nextLink);
+        return [content, next_page_link];
+      } else {
+        console.error(
+          `Error fetching content from ${page_link}. Status code: ${response.status}`
+        );
+      }
     } catch (error) {
-        console.error(`Error fetching content from ${url}:`, error);
+      console.error(`Error fetching content from ${page_link}:`, error);
     }
   }
 
   function create_pagination(page_num, page_link) {
-    const html=`
+    const html = `
         <div class="sp-separator" id="sp-separator-${page_num}">
         <a class="sp-sp-nextlink" target="_blank" href="${page_link}" title="${page_link}">
           <b>Page <span style="color:#595959!important;">${page_num}</span></b>
           [ Actual elements/pages: <span style="color:#595959!important;">${page_num}</span> ]
         </a>
         </div>`;
-    const template = document.createElement('template');
+    const template = document.createElement("template");
     template.innerHTML = html;
     const result = template.content.children;
     if (result.length === 1) return result[0];
@@ -78,70 +83,242 @@
         background: none !important;
         color: #595959 !important;
       }`;
-    const styleElement = doc.createElement('style');
-    styleElement.type = 'text/css';
+    const styleElement = doc.createElement("style");
+    styleElement.type = "text/css";
     styleElement.appendChild(doc.createTextNode(customStyles));
 
     // Append the <style> element to the <head>
     doc.head.appendChild(styleElement);
-
   }
 
-  async function main(nextLink, pageElement) {
+  async function fetch_all(nextLinks, pageElement) {
     await timeout(1000);
-    const pageSelector = nextLink;
     const contentSelector = pageElement;
     const nextPages = [];
-    document.querySelectorAll(pageSelector).forEach(link => {
-        const hrefValue = link.getAttribute('href');
-        nextPages.push(hrefValue);
+    document.querySelectorAll(nextLinks).forEach((link) => {
+      const hrefValue = link.getAttribute("href");
+      nextPages.push(hrefValue);
     });
-    console.log(`Next pages: ${nextPages}`)
-    const parsedDOMs = await Promise.all(nextPages.map((url)=>fetchContent(url, contentSelector)));
+    console.log(`Next pages: ${nextPages}`);
+    const parsedDOMs = await Promise.all(
+      nextPages.map(async (url) => {
+        const [content, page_url] = await fetchContent(url, contentSelector);
+        return content;
+      })
+    );
 
     const targetElement = document.querySelector(contentSelector);
     add_pagination_style(document);
 
     parsedDOMs.forEach((parsedDOM, idx) => {
-        if (parsedDOM) {
-          while (parsedDOM.firstChild) {
-              targetElement.appendChild(parsedDOM.firstChild);
-          }
-          const pagination = create_pagination(idx+1, nextPages[idx]);
-          targetElement.appendChild(pagination);
+      if (parsedDOM) {
+        const pagination = create_pagination(idx + 1, nextPages[idx]);
+        targetElement.appendChild(pagination);
+        while (parsedDOM.firstChild) {
+          targetElement.appendChild(parsedDOM.firstChild);
         }
+      }
     });
+  }
 
+  class InfiniteScrollLoader {
+    constructor(contentSelector, nextLinkSelector, paginationSelector) {
+      this.contentSelector = contentSelector;
+      this.nextLinkSelector = nextLinkSelector;
+      this.paginationSelector = paginationSelector || nextLinkSelector;
+      this.updateElement();
+      this.observer = null;
+
+      const nextPageLink = this.nextLinkElement.getAttribute("href");
+      if (nextPageLink) {
+        console.info(`Prefetch ${nextPageLink}`);
+        this.request = fetch(nextPageLink).then((response) => response.text());
+        this.initObserver();
+      } else {
+        console.log("Don't find next page link");
+      }
+    }
+
+    updateElement() {
+      this.contentElement = document.querySelector(this.contentSelector);
+      this.nextLinkElement = document.querySelector(this.nextLinkSelector);
+      this.paginationElement = document.querySelector(this.paginationSelector);
+    }
+
+    // Initialize the IntersectionObserver
+    initObserver() {
+      const options = {
+        root: null, // Viewport
+        rootMargin: "100px 0px",
+        threshold: 1.0, // Trigger when the element is 100% visible
+      };
+
+      this.observer = new IntersectionObserver(
+        this.handleIntersection.bind(this),
+        options
+      );
+      this.observer.observe(this.nextLinkElement);
+    }
+
+    // Intersection handler
+    handleIntersection(entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.loadMoreContent();
+        }
+      });
+    }
+
+    // Fetch and append new content, and replace pagination element
+    loadMoreContent() {
+      this.request
+        .then((html) => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+
+          // Extract new content
+          const newContent = doc.querySelector(this.contentSelector);
+          if (newContent) {
+            // Append each child of the new content to the current content element
+            Array.from(newContent.children).forEach((child) => {
+              this.contentElement.appendChild(child);
+            });
+          }
+
+          // Extract and replace the pagination element
+          const newNextLinkElement = doc.querySelector(this.nextLinkSelector);
+          const newPaginationElement = doc.querySelector(
+            this.paginationSelector
+          );
+          if (newNextLinkElement && newPaginationElement) {
+            this.observer.unobserve(this.nextLinkElement);
+
+            if (this.nextLinkSelector != this.paginationSelector) {
+              this.paginationElement.replaceWith(newPaginationElement);
+            } else {
+              this.nextLinkElement.setAttribute(
+                "href",
+                newNextLinkElement.getAttribute("href")
+              );
+            }
+            this.updateElement();
+
+            const nextPageLink = this.nextLinkElement.getAttribute("href");
+            if (nextPageLink) {
+              console.info(`Prefetch ${nextPageLink}`);
+              this.request = fetch(nextPageLink).then((response) =>
+                response.text()
+              );
+              // Reobserve the new pagination element
+              this.observer.observe(this.nextLinkElement);
+            } else {
+              console.log("Don't find next page link");
+            }
+          } else {
+            // If no new pagination element is found, stop observing
+            this.observer.unobserve(this.nextLinkElement);
+            // this.paginationElement.style.display = 'none'; // Optionally hide the pagination element
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          this.observer.unobserve(this.nextLinkElement);
+          return;
+        });
+    }
   }
 
   addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed, init pager");
     const database = [
-        {
-            name: 'xiutaku',
-            keyword: 'xiutaku.',
-            nextLink: 'nav:first-of-type span:has(a.pagination-link.is-current) ~span a',
-            pageElement: 'div.article-fulltext',
+      {
+        name: "xiutaku",
+        url: "^https?://(www\\.)?xiutaku\\.(com|[^/]*workers\\.dev)/\\d+(\\?page=\\d+)?$",
+        nextLinks:
+          "nav:first-of-type span:has(a.pagination-link.is-current) ~span a",
+        pageElement: "div.article-fulltext",
+      },
+      {
+        name: "xiutaku post list",
+        url: "^https?://(www\\.)?xiutaku\\.(com|[^/]*workers\\.dev)/([^\\d].*)?$",
+        nextLink: "a.pagination-next",
+        pagination: "nav.pagination",
+        pageElement: "div.blog ~.blog",
+      },
+      {
+        name: "4khd",
+        url: "^https?://(www\\.)?4khd\\.(com|[^/]*workers\\.dev)/.*\\.html$",
+        nextLinks: "li.current ~ li a",
+        pageElement: "div.wp-block-post-content > p",
+      },
+      {
+        name: "4khd post list",
+        url: "^https?://(www\\.)?4khd\\.(com|[^/]*workers\\.dev)(/(pages|search|\\?query).*)?",
+        nextLink: "span.current~a",
+        pagination: "nav.wp-block-query-pagination",
+        pageElement: "ul:has(li.wp-block-post)",
+      },
+      {
+        name: "jpxgmn",
+        url: "",
+        head: {
+          title: ".*xgmn.*",
         },
-        {
-            name: '4khd',
-            keyword: '4khd.',
-            nextLink: 'li.current ~ li a',
-            pageElement: 'div.wp-block-post-content > p',
-        }
-    ]
+      },
+    ];
 
-    let d = null;
-    for (const i of database) {
-        if (location.href.includes(i.keyword)) {
-            d = i;
-            break
+    let config = null;
+    for (const this_config of database) {
+      let match = false;
+      if ("url" in this_config && this_config["url"].length >= 0) {
+        const url_re = new RegExp(this_config["url"]);
+        if (url_re.test(location.href)) {
+          match = true;
         }
+      } else if ("head" in this_config) {
+        for (const key in this_config.head) {
+          let selector = "";
+          const value_re = new RegExp(this_config.head[key]);
+          if (key === "title") {
+            selector = "head title";
+          } else if (key.startsWith("meta")) {
+            const [_, name] = key.split(".", 2);
+            selector = `head meta[name="${name}"]`;
+          }
+          const element = document.querySelector(selector);
+          if (element) {
+            if (key === "title" && value_re.test(element.innerHTML)) {
+              match = true;
+            } else if (key.startsWith("meta")) {
+              const content = element.getAttribute("content");
+              if (value_re.test(content)) {
+                match = true;
+              }
+            }
+          }
+        }
+      }
+      if (match) {
+        config = this_config;
+        break;
+      }
     }
-    if (d) {
-        main(d.nextLink, d.pageElement).then(()=>{console.log("Autopager is done")});
+    if (config) {
+      if ("nextLinks" in config) {
+        console.log("Autopager fetch-all mode");
+        fetch_all(config.nextLinks, config.pageElement).then(() => {
+          console.log("Autopager is done");
+        });
+      } else if ("nextLink" in config) {
+        console.log("Autopager next-page mode");
+        new InfiniteScrollLoader(
+          config.pageElement, // Selector for the content container
+          config.nextLink, // Selector for the pagination link within the pagination bar
+          config.pagination // Pagination bar to replace
+        );
+      }
     } else {
-        console.error('No rule is found');
+      console.error("No rule is found");
     }
   });
 })();
